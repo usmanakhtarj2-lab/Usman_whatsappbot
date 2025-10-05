88fd734a5d9c774ab2373b17719bf781dd77ceb8
usmanakhtarj2-lab <usmanakhtarj2@gmail.com>
2025-10-05 15:47:14 +0500

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -38,14 +38,14 @@ async (conn, mek, m, { from }) => {
 ┃◈┃• *Version*: 2.0.0 Beta
 ┃◈└───────────┈⊷
 ╰──────────────┈⊷
-> *© ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝐀𝐫𝐬𝐥𝐚𝐧_𝐌𝐃 ❣️*`, // Display the owner's details
+> *© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Usman-Akhtar-Bot ❣️*`, // Display the owner's details
             contextInfo: {
                 mentionedJid: [`${ownerNumber.replace('+', '')}@s.whatsapp.net`], 
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '0029Vb5ykzpFcowDtdEiIM3K@newsletter',
-                    newsletterName: '*Usman-Akhtar-Bot*',
+                    newsletterName: 'Usman-Akhtar-Bot',
                     serverMessageId: 143
                 }            
             }
